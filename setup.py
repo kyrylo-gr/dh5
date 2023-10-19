5cb2a1fd838cd5b751a92a0f2d38780d8dae5eed
Kirill <4cd87a@gmail.com>
2023-10-19 23:08:48 +0200

    version 0.7.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dh5",
-    version="0.8.0",
+    version="0.7.8",
     author="LKB-OMQ",
     author_email="cryo.paris.su@gmail.com",
     description="Data H5. Work with hdf5 as classical dictionary.",
